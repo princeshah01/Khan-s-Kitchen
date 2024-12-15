d1676a212f692a628e99c9b8fcb46bac3c974a5d
princeshah01 <prince.rjb839@gmail.com>
2024-12-15 17:16:01 +0530

    fixed search problem

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,49 +2,60 @@ import Card from './MenuCard';
 import Simmer from './Simmer';
 import SearchBar from './SearchBar';
 import { foodDataMain } from './utils/data';
-import { useState ,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 
 const Menu = () => {
   let [MainMenu, setMainMenu] = useState([]);
+  let [originalMenu , SetoriginalMenu] = useState([]) ;
   let [query, setQuery] = useState('');
 
-  
-      useEffect( () =>{
-        fetchData();
-      },[]);
-    
-        let fetchData = async ()=>{
-          let data = await foodDataMain();
-          console.log(data);
-          setMainMenu(data) ;
-        }
-    
-      const handleSearch = (e) => {
-        const q = e.target.value;
-        setQuery(q); 
-    
-       
-        const filtered = foodDataMain.filter((item) =>item.name.toLowerCase().includes(q.toLowerCase()));
-        setMainMenu(filtered); 
-      };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  let fetchData = async () => {
+    let data = await foodDataMain();
+    console.log(data);
+    SetoriginalMenu(data) ;
+    setMainMenu(data);
+  }
+
+  const handleSearch = (e) => {
+    const q = e.target.value;
+    setQuery(q);
 
 
-  if(MainMenu.length === 0 ){
-    
+    const filtered = originalMenu.filter((item) => item.name.toLowerCase().includes(q.toLowerCase()));
+    setMainMenu(filtered);
+  };
+
+
+  if (originalMenu.length === 0) {
+
     return (
       <div>
-              <SearchBar/>
+        <SearchBar 
+               setMainMenu={setMainMenu}
+               handleSearch={handleSearch}
+               setQuery={setQuery}
+               query={query}
+        />
 
-    <Simmer  />
+        <Simmer />
       </div>
     )
   }
 
   return (
     <div className='flex flex-col gap-4 w-full'>
-     
-    <SearchBar props={{setMainMenu,handleSearch,setQuery,query}}/>
+
+      <SearchBar setMainMenu={setMainMenu}
+        handleSearch={handleSearch}
+        setQuery={setQuery}
+        query={query}
+      />
 
       <div className='w-[80%] place-items-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 mx-8 my-16 md:mx-16 lg:mx-32'>
         {MainMenu?.length > 0 ? (
@@ -52,7 +63,10 @@ const Menu = () => {
             <Card key={index} food={data} />
           ))
         ) : (
-          <p className="text-center text-lg text-gray-500">No food items found</p>
+          <div className='col-span-full grid place-items-center h-40'>
+
+            <p className="text-lg text-gray-500">No food items found</p>
+          </div>
         )}
       </div>
     </div>
